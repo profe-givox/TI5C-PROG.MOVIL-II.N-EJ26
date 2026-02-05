f4221957555b4c98ce9293797adcfd87b947bcd5
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-02-05 13:43:13 -0600

    se finalizo el worker, se armo el workrequest, se encolo el trabajo.

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -3,8 +3,15 @@ package com.example.bluromatic.workers
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.example.bluromatic.DELAY_TIME_MILLIS
 import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
+private const val TAG = "BlurWorker"
 class BlurWorker(ctx: Context, params: WorkerParameters)
     : CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
@@ -12,10 +19,31 @@ class BlurWorker(ctx: Context, params: WorkerParameters)
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
         )
-        return try {
-            Result.success()
-        } catch (throwable: Throwable) {
-            Result.failure()
+        return withContext(Dispatchers.IO) {
+
+            return@withContext try {
+                delay(DELAY_TIME_MILLIS)
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+                val output = blurBitmap(picture, 1)
+                val outputUri = writeBitmapToFile(applicationContext, output)
+
+                makeStatusNotification(
+                    "Output is $outputUri",
+                    applicationContext
+                )
+
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
         }
     }
 }
